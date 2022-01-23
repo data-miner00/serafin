e31c0f2e5a1f114a2b2ee22c1e6597ff3b814d5b
data-miner00 <data-miner00@users.noreply.github.com>
2022-01-23 19:18:56 +0800

    feat: Working discord bot

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,70 @@
-use std::env;
+extern crate dotenv;
+extern crate serenity;
 
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use dotenv::dotenv;
+
+use serenity::async_trait;
+use serenity::client::{Client, Context, EventHandler};
+use serenity::model::gateway::Ready;
+use serenity::model::channel::Message;
+use serenity::framework::standard::{
+    StandardFramework,
+    CommandResult,
+    macros::{
+        command,
+        group
+    }
+};
+
+#[group]
+#[commands(ping)]
+struct General;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: Message) {
+        if msg.content == "!shard" {
+            println!("Shard {}", ctx.shard_id);
+
+            if let Err(why) = msg.channel_id.say(&ctx.http, "Poing!").await {
+                println!("error sending message: {:?}", why);
+            }
+        }
+    }
+
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+
+    let framework = StandardFramework::new()
+        .configure(|c| c.prefix("~")) // set the bot's prefix to "~"
+        .group(&GENERAL_GROUP);
+
+    // Login with a bot token from the environment
+    let token = env::var("DISCORD_API_TOKEN").expect("token");
+    let mut client = Client::builder(token)
+        .event_handler(Handler)
+        .framework(framework)
+        .await
+        .expect("Error creating client");
+
+    // start listening for events by starting a single shard
+    if let Err(why) = client.start().await {
+        println!("An error occurred while running the client: {:?}", why);
+    }
+}
+
+#[command]
+async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.reply(ctx, "Pong!").await?;
+
+    Ok(())
 }
